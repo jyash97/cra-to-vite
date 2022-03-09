d6ead32d6bffbf1ba826f7c6f80c4f41457b3398
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-03-09 14:53:08 +0100

    add formating

--- src/utils.js
+++ src/utils.js
@@ -34,8 +34,8 @@ function getPackageJSON() {
 function getViteConfig() {
   return {
     content: `
-    import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
       
     export default defineConfig({
         plugins: [react()]
